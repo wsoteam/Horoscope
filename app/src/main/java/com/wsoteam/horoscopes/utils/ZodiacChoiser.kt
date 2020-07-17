9305c4c11589a2cf9805daf1abb7168f61a2d03d
Wadim <waadimka@mail.ru>
2020-07-17 13:06:28 +0300

    Update ZodiacChoiser.kt

--- app/src/main/java/com/wsoteam/horoscopes/utils/ZodiacChoiser.kt
+++ app/src/main/java/com/wsoteam/horoscopes/utils/ZodiacChoiser.kt
@@ -2,7 +2,98 @@ package com.wsoteam.horoscopes.utils
 
 object ZodiacChoiser {
 
-    fun choiceSign(date : String){
+    fun choiceSign(date : String) : Int{
+        var day = date.split(".")[0].toInt()
+        var month = date.split(".")[1].toInt()
+        var signIndex = -1
 
+        when(month){
+            1 -> {
+                signIndex = if (day > 20){
+                    10
+                }else{
+                    9
+                }
+            }
+            2 -> {
+                signIndex = if (day > 18){
+                    11
+                }else{
+                    10
+                }
+            }
+            3 -> {
+                signIndex = if (day > 20){
+                    0
+                }else{
+                    11
+                }
+            }
+            4 -> {
+                signIndex = if (day > 20){
+                    1
+                }else{
+                    0
+                }
+            }
+            5 -> {
+                signIndex = if (day > 21){
+                    2
+                }else{
+                    1
+                }
+            }
+            6 -> {
+                signIndex = if (day > 21){
+                    3
+                }else{
+                    2
+                }
+            }
+            7 -> {
+                signIndex = if (day > 22){
+                    4
+                }else{
+                    3
+                }
+            }
+            8 -> {
+                signIndex = if (day > 23){
+                    5
+                }else{
+                    4
+                }
+            }
+            9 -> {
+                signIndex = if (day > 23){
+                    6
+                }else{
+                    5
+                }
+            }
+            10 -> {
+                signIndex = if (day > 22){
+                    7
+                }else{
+                    6
+                }
+            }
+            11 -> {
+                signIndex = if (day > 22){
+                    8
+                }else{
+                    7
+                }
+            }
+            12 -> {
+                signIndex = if (day > 21){
+                    9
+                }else{
+                    8
+                }
+            }
+        }
+
+        return signIndex
     }
 }
