072eaf87df4d219bae48d2e5c499fe73f6f11f5f
Wadim <waadimka@mail.ru>
2020-07-23 15:31:20 +0300

    add res

---
+++ app/src/main/java/com/wsoteam/horoscopes/presentation/main/MainFragment.kt
@@ -0,0 +1,7 @@
+package com.wsoteam.horoscopes.presentation.main
+
+import androidx.fragment.app.Fragment
+import com.wsoteam.horoscopes.R
+
+class MainFragment : Fragment(R.layout.main_fragment) {
+}
