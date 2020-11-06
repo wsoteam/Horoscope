d09111f2c54a6893d3dac5e31dbbf0cdf8ce580f
Wadim <udovoron@gmail.com>
2020-11-06 13:57:20 +0300

    finish activity

---
+++ app/src/main/java/com/wsoteam/horoscopes/presentation/onboarding/FinishActivity.kt
@@ -0,0 +1,7 @@
+package com.wsoteam.horoscopes.presentation.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import com.wsoteam.horoscopes.R
+
+class FinishActivity : AppCompatActivity(R.layout.finish_activity) {
+}
