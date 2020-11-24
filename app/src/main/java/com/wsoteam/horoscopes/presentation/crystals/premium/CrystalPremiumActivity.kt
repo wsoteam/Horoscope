8586df20ee3a26e3466205ace47ca84278991d69
Wadim <udovoron@gmail.com>
2020-11-24 14:04:44 +0300

    add prem activity

---
+++ app/src/main/java/com/wsoteam/horoscopes/presentation/crystals/premium/CrystalPremiumActivity.kt
@@ -0,0 +1,7 @@
+package com.wsoteam.horoscopes.presentation.crystals.premium
+
+import androidx.appcompat.app.AppCompatActivity
+import com.wsoteam.horoscopes.R
+
+class CrystalPremiumActivity : AppCompatActivity(R.layout.crystal_premium_activity) {
+}
