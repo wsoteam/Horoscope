94da433cc854b9ff4f64e44b7af83d249d64c3b3
Wadim <udovoron@gmail.com>
2020-11-24 17:46:37 +0300

    dialogs

---
+++ app/src/main/java/com/wsoteam/horoscopes/presentation/crystals/main/dialogs/EndDialog.kt
@@ -0,0 +1,26 @@
+package com.wsoteam.horoscopes.presentation.crystals.main.dialogs
+
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.wsoteam.horoscopes.R
+
+class EndDialog : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = inflater.inflate(R.layout.end_dialog, container, false)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(0))
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
---
+++ app/src/main/java/com/wsoteam/horoscopes/presentation/crystals/premium/dialogs/PremiumSuccessDialog.kt
@@ -0,0 +1,26 @@
+package com.wsoteam.horoscopes.presentation.crystals.premium.dialogs
+
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.wsoteam.horoscopes.R
+
+class PremiumSuccessDialog : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = inflater.inflate(R.layout.premium_success_dialog, container, false)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(0))
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
