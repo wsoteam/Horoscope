21d52595cd87dc548ab913cb53dd63fdacfff397
Wadim <udovoron@gmail.com>
2020-12-11 14:44:47 +0300

    fix

--- app/src/main/java/com/wsoteam/horoscopes/utils/remote/ABConfig.kt
+++ app/src/main/java/com/wsoteam/horoscopes/utils/remote/ABConfig.kt
@@ -9,8 +9,9 @@ object ABConfig {
     const val C = "onboard"
     const val D = "alert"
     const val E = "ondoard_space"
-    const val F = "ondoard_girl"
-    const val G = "ondoard_diamond"
+    const val F = "ondoard_diamond"
+    const val G = "ondoard_girl"
+
 
     const val REQUEST_STRING_PRICE = "prices"
 }
