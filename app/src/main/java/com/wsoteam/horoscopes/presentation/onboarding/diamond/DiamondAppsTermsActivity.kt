fbe76b2fe22751105176bff32dcb402e343b27cf
Wadim <40279063+wkazak@users.noreply.github.com>
2020-12-11 14:07:02 +0300

    ui diamond

---
+++ app/src/main/java/com/wsoteam/horoscopes/presentation/onboarding/diamond/DiamondAppsTermsActivity.kt
@@ -0,0 +1,53 @@
+package com.wsoteam.horoscopes.presentation.onboarding.diamond
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.wsoteam.horoscopes.Config
+import com.wsoteam.horoscopes.R
+import com.wsoteam.horoscopes.presentation.form.FormActivity
+import com.wsoteam.horoscopes.utils.InAppCallback
+import com.wsoteam.horoscopes.utils.PreferencesProvider
+import com.wsoteam.horoscopes.utils.PriceManager
+import com.wsoteam.horoscopes.utils.SubscriptionProvider
+import com.wsoteam.horoscopes.utils.analytics.Analytic
+import com.wsoteam.horoscopes.utils.analytics.FBAnalytic
+import kotlinx.android.synthetic.main.app_terms_activity.*
+
+class DiamondAppsTermsActivity : AppCompatActivity(R.layout.diamond_app_terms_activity) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Analytic.showPrem("old_onboard_app_terms")
+        ivClose.setOnClickListener {
+            openNext()
+        }
+
+        btnPay.setOnClickListener { _ ->
+            SubscriptionProvider.startChoiseSub(this, Config.ONBOARD_SUB, object :
+                InAppCallback {
+                override fun trialSucces() {
+                    handlInApp()
+                }
+            })
+        }
+
+    }
+
+    private fun openNext(){
+        startActivity(Intent(this, PrivacyPoliceActivity::class.java))
+    }
+
+    private fun handlInApp() {
+        Analytic.makePurchase(PreferencesProvider.getVersion()!!, "form")
+        Analytic.makePurchaseFromOnboard("old_onboard_app_terms")
+        FirebaseAnalytics.getInstance(this).logEvent("trial", null)
+        FBAnalytic.logTrial(this)
+        PreferencesProvider.setADStatus(false)
+        startActivity(Intent(this, FormActivity::class.java))
+        finishAffinity()
+    }
+
+
+}
