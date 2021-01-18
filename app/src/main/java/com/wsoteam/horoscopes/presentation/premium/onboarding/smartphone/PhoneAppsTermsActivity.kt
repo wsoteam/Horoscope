1130a1fa7dae9f780e1029b4fdd7ecb93bc3309e
Wadim <udovoron@gmail.com>
2021-01-18 14:21:39 +0300

    add phone activities

---
+++ app/src/main/java/com/wsoteam/horoscopes/presentation/premium/onboarding/smartphone/PhoneAppsTermsActivity.kt
@@ -0,0 +1,7 @@
+package com.wsoteam.horoscopes.presentation.premium.onboarding.smartphone
+
+import androidx.appcompat.app.AppCompatActivity
+import com.wsoteam.horoscopes.R
+
+class PhoneAppsTermsActivity : AppCompatActivity(R.layout.phone_apps_terms_activity) {
+}
---
+++ app/src/main/java/com/wsoteam/horoscopes/presentation/premium/onboarding/smartphone/PhoneFinishActivity.kt
@@ -0,0 +1,7 @@
+package com.wsoteam.horoscopes.presentation.premium.onboarding.smartphone
+
+import androidx.appcompat.app.AppCompatActivity
+import com.wsoteam.horoscopes.R
+
+class PhoneFinishActivity : AppCompatActivity(R.layout.phone_finish_activity) {
+}
