1f70e5e20b3e28188d4a6edecbcf8f24ca5eecee
Wadim <40245987+vsalamov@users.noreply.github.com>
2021-06-14 13:08:22 +0300

    rel

--- app/src/main/java/com/wsoteam/horoscopes/presentation/onboard/prem/EnterActivity.kt
+++ app/src/main/java/com/wsoteam/horoscopes/presentation/onboard/prem/EnterActivity.kt
@@ -2,6 +2,7 @@ package com.wsoteam.horoscopes.presentation.onboard.prem
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -53,6 +54,7 @@ class EnterActivity : AppCompatActivity(R.layout.enter_prem_activity) {
                 }
             })
         }
+
     }
 
     private fun setUserData() {
