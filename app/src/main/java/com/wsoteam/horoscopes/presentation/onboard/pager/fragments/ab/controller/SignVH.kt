2e49032800a5f2b601fc2436740f1657f6f35e58
Wadim <40245987+vsalamov@users.noreply.github.com>
2021-06-25 11:57:00 +0300

    fix
    - cache dialog imgs
    - ui settings

--- app/src/main/java/com/wsoteam/horoscopes/presentation/onboard/pager/fragments/ab/controller/SignVH.kt
+++ app/src/main/java/com/wsoteam/horoscopes/presentation/onboard/pager/fragments/ab/controller/SignVH.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.wsoteam.horoscopes.R
 import kotlinx.android.synthetic.main.sign_vh.view.*
 
@@ -23,7 +24,7 @@ class SignVH(layoutInflater: LayoutInflater, viewGroup: ViewGroup, var iChoiseSi
 
     fun bind(img: Int, name: String, isSelected : Boolean) {
         itemView.tvSign.text = name
-        itemView.ivSign.setImageResource(img)
+        Glide.with(itemView).load(img).into(itemView.ivSign)
 
         if(isSelected){
             itemView.ivSelector.visibility = View.VISIBLE
